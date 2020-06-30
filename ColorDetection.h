761193dd3cd44febba8614f142842ac402b47b1e
Nguyen Anh Tuan <55679990+K18rs043@users.noreply.github.com>
2020-06-30 14:51:39 +0900

    順番をcaseの順番に合わせた

--- ColorDetection.h
+++ ColorDetection.h
@@ -1,9 +1,9 @@
 //ColorDetection.h
 
-#define WHITE           1       //白
+#define WHITE           4       //白
 #define BLACK           2       //黒
 #define RED             3       //赤
-#define GRAY            4       //灰色
+#define GRAY            1       //灰色
 
 //色検出
 int DetectionColor(int color_sensor);
