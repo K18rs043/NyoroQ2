9f995d400398a734d0651c3d3df421d0b387411c
Nguyen Anh Tuan <55679990+K18rs043@users.noreply.github.com>
2020-06-30 14:25:23 +0900

    定義を追加した

--- RunControl.h
+++ RunControl.h
@@ -5,3 +5,7 @@ void forward(int left_motor, int right_motor, int speed);
 
 //モータ停止
 void stop(int left_motor, int right_motor);
+
+//カーブ
+void turnLeft(int left_motor, int right_motor, int speed,int rightspeed);
+void turnRight(int left_motor, int right_motor, int speed,int leftspeed);
