b496f243fc4c76b2bb22c1755cac9f18a40f38f8
Nguyen Anh Tuan <55679990+K18rs043@users.noreply.github.com>
2020-06-30 14:58:14 +0900

    速度の変数
    ーカーブするときの速度の引き数名を1つにした
    ーコメントを追加した

--- RunControl.h
+++ RunControl.h
@@ -7,5 +7,5 @@ void forward(int left_motor, int right_motor, int speed);
 void stop(int left_motor, int right_motor);
 
 //カーブ
-void turnLeft(int left_motor, int right_motor, int speed,int rightspeed);
-void turnRight(int left_motor, int right_motor, int speed,int leftspeed);
+void turnLeft(int left_motor, int right_motor, int speed,int turnspeed); //左カーブ
+void turnRight(int left_motor, int right_motor, int speed,int turnspeed); //右カーブ
